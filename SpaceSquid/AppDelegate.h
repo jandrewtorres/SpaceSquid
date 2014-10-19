5f035d1822e2fb090ed0132b4d81c20ee30742b7
Andrew Torres <AndrewTorres@Andrews-MacBook-Pro.local>
2014-10-19 15:13:00 -0700

    Initial Commit

---
+++ SpaceSquid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpaceSquid
+//
+//  Created by Andrew Torres on 10/19/14.
+//  Copyright (c) 2014 Andrew Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpaceSquid/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SpaceSquid
+//
+
+//  Copyright (c) 2014 Andrew Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
